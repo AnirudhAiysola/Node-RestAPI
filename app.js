f9f86ee5dd5850afca8e0fc796140228e4989f26
anirudhaiysola <anirudhisola1011@gmail.com>
2024-02-13 21:38:41 +0530

    main-commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  `mongodb+srv://dummyuser:dummy123@cluster0.asrxisc.mongodb.net/`
+);
+
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/users");
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Headers", "PUT, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/users", userRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
